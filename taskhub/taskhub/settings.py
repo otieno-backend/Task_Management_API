257af4a500b58f4cd957cba860bef0989dbe47eb
Otibra <otienobrian718@gmail.com>
2026-06-07 08:36:15 -0700

    Add deployment platform configuration

--- taskhub/taskhub/settings.py
+++ taskhub/taskhub/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://task-management-api-wpw5.onrender.com']
+ALLOWED_HOSTS = [
+    "task-management-api-wpw5.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
